f2827139e12988476a3ac835c177fe26dfe7528d
Alden <chris.alden234@gmail.com>
2023-02-21 19:19:26 +0800

    second init

--- api.py
+++ api.py
@@ -4,4 +4,4 @@ from urllib.request import urlopen
 with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json") as response:
     source = response.read()
   
-print(source)
+print("hello")
